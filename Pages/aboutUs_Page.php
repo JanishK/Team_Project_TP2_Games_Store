5be775fcfb1bf435bf58b18b6e9995785ec7098b
Janish Kandel <janishkuk@gmail.com>
2026-03-04 14:17:49 +0000

    Updated the nav bar and now it adds a admin panel when the user is admin and it removes when its logged out and also there is just normal links to create account etc, when you first load up you are a guest and you can go and login

--- Pages/aboutUs_Page.php
+++ Pages/aboutUs_Page.php
@@ -20,51 +20,8 @@ require_once('themes.php');
 <body class="<?php echo $themeClass; ?>">
   <!-- NAV -->
             <!-- NAVIGATION BAR -->
-    <nav class="cb-nav">
-        <div class="cb-nav__container">
-            
-            <!-- Brand -->
-            <a class="cb-brand" href="./home_Page.html">
-            <img class="cb-brand__logo" src="/Team_Project_TP2_Games_Store/Assets/Logo.png" alt="CoreByte Logo" />
-            <span class="cb-brand__text">CoreByte</span>
-            </a>
+    <?php require_once __DIR__ . '/components/navbar.php'; ?>
 
-            <!-- Main links -->
-            <ul class="cb-links" id="cbNavLinks">
-                <li><a href="./home_Page.php" class="cb-link is-active">Home</a></li>
-                <li><a href="./Products_Page.php" class="cb-link">Products</a></li>
-                <li><a href="./aboutUs_Page.php" class="cb-link">About</a></li>
-            </ul>
-
-            <!-- User avatar dropdown -->
-            <div class="cb-user">
-            <button class="cb-user__btn" type="button" id="cbUserBtn" aria-expanded="false" aria-controls="cbUserMenu">
-                <span class="sr-only">Open user menu</span>
-                <img
-                class="cb-user__avatar"
-                src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                alt="User photo"
-                />
-            </button>
-
-
-
-            <div class="cb-user__menu hidden" id="cbUserMenu" role="menu">
-                <div class="cb-user__header">
-                <span class="cb-user__name">Janish Kandel</span>
-                <span class="cb-user__email">JanishK@corebyte.com</span>
-                </div>
-
-                <a href="./basket_Page.php" role="menuitem">Basket <span class="notification">1</span></a>
-                <a href="./registration_page.php" role="menuitem">Account</a>
-                <a href="./settingsPage.php" role="menuitem">Settings</a>
-                <a href="./contactUs_Page.php" role="menuitem">Support</a>
-                <a href="#" role="menuitem">Sign out</a>
-            </div>
-            </div>
-
-        </div>
-    </nav>
    
 
   <!-- PAGE -->
--- Pages/settingsPage.php
+++ Pages/settingsPage.php
@@ -100,42 +100,8 @@ try {
 <body class="<?php echo $themeClass; ?>">
 
      <!-- NAVIGATION BAR (your cb-nav) -->
-  <nav class="cb-nav">
-    <div class="cb-nav__container">
+      <?php require_once __DIR__ . '/components/navbar.php'; ?>
 
-      <a class="cb-brand" href="./home_Page.php">
-        <img class="cb-brand__logo" src="/Team_Project_TP2_Games_Store/Assets/Logo.png" alt="CoreByte Logo" />
-        <span class="cb-brand__text">CoreByte</span>
-      </a>
-
-      <ul class="cb-links" id="cbNavLinks">
-        <li><a href="./home_Page.php" class="cb-link">Home</a></li>
-        <li><a href="./Products_Page.php" class="cb-link is-active">Products</a></li>
-        <li><a href="./aboutUs_Page.php" class="cb-link">About</a></li>
-      </ul>
-
-      <div class="cb-user">
-        <button class="cb-user__btn" type="button" id="cbUserBtn" aria-expanded="false" aria-controls="cbUserMenu">
-          <span class="sr-only">Open user menu</span>
-          <img class="cb-user__avatar" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="User photo" />
-        </button>
-
-        <div class="cb-user__menu hidden" id="cbUserMenu" role="menu">
-          <div class="cb-user__header">
-            <span class="cb-user__name">Janish Kandel</span>
-            <span class="cb-user__email">JanishK@corebyte.com</span>
-          </div>
-
-          <a href="./basket_Page.php" role="menuitem">Basket</a>
-          <a href="./registration_page.php" role="menuitem">Account</a>
-          <a href="./settingsPage.php" role="menuitem">Settings</a>
-          <a href="./contactUs_Page.php" role="menuitem">Support</a>
-          <a href="#" role="menuitem">Sign out</a>
-        </div>
-      </div>
-
-    </div>
-  </nav>
 
   <!-- Page content -->
   <main class="settings-container">
